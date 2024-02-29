83f9cf40ead71e2a2d5ebbc474ca4014c92b4a1e
dimel035 <45945660+dimel035@users.noreply.github.com>
2024-02-29 22:47:56 +0200

    fixed follow camera

--- 2D-platformer/Assets/Scripts/CameraController.cs
+++ 2D-platformer/Assets/Scripts/CameraController.cs
@@ -13,18 +13,19 @@ public class CameraController : MonoBehaviour
 
     //Follow Player
     [SerializeField] private Transform player;
-    [SerializeField] private float addDistance;
     [SerializeField] private float cameraSpeed;
-    private float lookAhead;
+    [SerializeField] public float yOffset = 1f;
+
+    
 
     private void Update()
     {
         //Room Camera
-        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y, transform.position.z), ref velocity,speed);
+        /*transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y, transform.position.z), ref velocity,speed);*/
 
         //Follow Player
-        transform.position = new Vector3(player.position.x+lookAhead, transform.position.y, transform.position.z);
-        lookAhead = Mathf.Lerp(lookAhead,(addDistance*player.localScale.x), Time.deltaTime*cameraSpeed);  
+        Vector3 newPos = new Vector3(player.position.x, player.position.y+yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, cameraSpeed*Time.deltaTime);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
