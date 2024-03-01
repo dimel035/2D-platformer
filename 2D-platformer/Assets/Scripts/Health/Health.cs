be5f5078e7d9a88d2f0938f8cc02eb83a90efef9
dimel035 <45945660+dimel035@users.noreply.github.com>
2024-03-01 11:42:24 +0200

    Basic health system added

---
+++ 2D-platformer/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   [SerializeField] private float startingHealth;
+
+    public float currentHealth {get; private set;}
+    private Animator anim;
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        if(currentHealth > 0)
+        {
+            //player hurt
+            anim.SetTrigger("hurt");
+            //iframes
+
+        }
+        else
+        {
+            if (!dead)
+            {
+                //player dead
+                anim.SetTrigger("die");
+                GetComponent<PMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+}
