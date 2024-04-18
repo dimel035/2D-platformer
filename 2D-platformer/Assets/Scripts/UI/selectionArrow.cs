5a5773878febe5d3d2e1bdfd63ce3f08804138da
dimel035 <45945660+dimel035@users.noreply.github.com>
2024-04-18 10:51:11 +0300

    gameOverScreen finished

--- 2D-platformer/Assets/Scripts/UI/selectionArrow.cs
+++ 2D-platformer/Assets/Scripts/UI/selectionArrow.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class selectionArrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private RectTransform[] options;
+    [SerializeField] private AudioClip movingSound;
+    [SerializeField] private AudioClip interactSound;
+
+    private RectTransform rect;
+    private int cPosition;
+
+
+    private void Awake()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) )
+        {
+            changePosition(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            changePosition(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Space))
+        {
+            OptionInteract();
+        }
     }
+
+    private void OptionInteract()
+    {
+        SoundManager.instance.PlaySound(interactSound);
+
+        options[cPosition].GetComponent<Button>().onClick.Invoke();
+
+    }
+
+    private void changePosition(int _change)
+    {
+        cPosition += _change;
+        if (_change != 0) 
+        {
+            SoundManager.instance.PlaySound(movingSound);
+        }
+
+        if(cPosition < 0)
+        {
+            cPosition = options.Length - 1;
+        }
+        else if(cPosition > options.Length - 1)
+        {
+            cPosition = 0;
+        }
+
+        rect.position = new Vector3(rect.position.x, options[cPosition].position.y, 0);
+    }
+
+
 }
